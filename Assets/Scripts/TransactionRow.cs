40b3ac8b6161461fe986aa37cc45f914f823e850
Joe Hansen <joe.hansen@mx.com>
2018-09-13 19:55:54 -0600

    Update Transaction Date and Description display

--- Assets/Scripts/TransactionRow.cs
+++ Assets/Scripts/TransactionRow.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
@@ -7,9 +8,8 @@ public class TransactionRow : MonoBehaviour {
 
   public GameObject background;
   public TextMeshPro dateText;
-  public TextMeshPro payeeText;
+  public TextMeshPro descriptionText;
   public TextMeshPro categoryText;
-  public TextMeshPro accountText;
   public TextMeshPro balanceText;
 
 	// Use this for initialization
@@ -17,18 +17,22 @@ public class TransactionRow : MonoBehaviour {
 		// Verify we have all required elements
 		Debug.Assert(background != null, "Missing Background Component");
 		Debug.Assert(dateText != null, "Missing Date Component");
-		Debug.Assert(payeeText != null, "Missing Payee Component");
+		Debug.Assert(descriptionText != null, "Missing Desciption Component");
 		Debug.Assert(categoryText != null, "Missing Category Component");
-		Debug.Assert(accountText != null, "Missing Account Component");
 		Debug.Assert(balanceText != null, "Missing Balance Component");
 	}
 
 	public void SetTransaction(Transaction transaction) {
 		_transaction = transaction;
-		dateText.text = transaction.created_at.ToString(); // TODO: Convert to date
-		payeeText.text = transaction.merchant_guid; // TODO: Look up Payee Name
+		if(_transaction.status == Transaction.Status.PENDING) {
+			dateText.text = "Pending";
+			// TODO: `Pending` should italicize fonts and use muted colors for all rows
+		} else {
+			var date = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc).AddSeconds(transaction.date);
+			dateText.text = date.ToString("MM/dd/y");
+		}
+		descriptionText.text = transaction.description;
 		categoryText.text = transaction.transaction_type.ToString();
-		accountText.text = transaction.account_guid; // TODO: Look up Account Name
 		balanceText.text = transaction.GetFormattedBalance();
 		balanceText.color = transaction.GetBalanceColor();
 	}
