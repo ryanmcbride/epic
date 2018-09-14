53c9dff5808b4cc13c94f50b0a55a4b3efdca62c
Rob Montgomery <rob.montgomery@mx.com>
2018-09-14 11:08:22 -0600

    Updating laserpointer script

--- Assets/Scripts/LaserPointer.cs
+++ Assets/Scripts/LaserPointer.cs
@@ -83,6 +83,10 @@ public class LaserPointer : MonoBehaviour
                 teleportReticleTransform.position = hitPoint + teleportReticleOffset;
 
                 shouldTeleport = true;
+            } else if(Physics.Raycast(trackedObj.transform.position, transform.forward, out hit, 100)) {
+                ShowLaser(hit);
+                shouldTeleport = false;
+                reticle.SetActive(false);
             } else {
                 laser.SetActive(false);
                 reticle.SetActive(false);
@@ -102,6 +106,10 @@ public class LaserPointer : MonoBehaviour
 
                 shouldWarp = true;
 
+            } else if(Physics.Raycast(trackedObj.transform.position, transform.forward, out hit, 100)) {
+                ShowLaser(hit);
+                shouldTeleport = false;
+                reticle.SetActive(false);
             } else {
                 laser.SetActive(false);
                 reticle.SetActive(false);
